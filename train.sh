5378f52b400a3e16b6d3b88d86ef894defff95b5
Jquinto64 <jquinto64@gmail.com>
2026-01-09 00:33:14 -0500

    updated bash scripts for Killarney

--- train.sh
+++ train.sh
@@ -1,34 +1,30 @@
 #!/bin/bash
-#SBATCH -p rtx6000            # partition: should be gpu on MaRS, and a40, t4v1, t4v2, or rtx6000 on Vaughan (v)
-#SBATCH --gres=gpu:4       # request GPU(s)
-#SBATCH -c 4              # number of CPU cores
-#SBATCH --mem=16G           # memory per node
+#SBATCH --gres=gpu:l40s:2   # request GPU(s)
+#SBATCH --cpus-per-task=8   # number of CPU cores
+#SBATCH --mem=20G           # memory per node
 #SBATCH --array=0           # array value (for running multiple seeds, etc)
-#SBATCH --qos=m2
-#SBATCH --time=8:00:00
+#SBATCH --time=12:00:00
 #SBATCH --output=slogs/%x_%A-%a_%n-%t.out
                             # %x=job-name, %A=job ID, %a=array value, %n=node rank, %t=task rank, %N=hostname
                             # Note: You must manually create output directory "slogs" 
 #SBATCH --open-mode=append  # Use append mode otherwise preemption resets the checkpoint file
 #SBATCH --job-name=mask_rcnn_lifeplan_b_512_sahi_tiled_v9_15k_iters_new_rotations
-#SBATCH --exclude=gpu175,gpu150
 
-
-source ~/.bashrc
-source activate mask_rcnn
-module load cuda-11.3
-
-SEED="$SLURM_ARRAY_TASK_ID"
+ENV_NAME=mask_rcnn
+module load StdEnv/2020
+module load python/3.10.2
+module load cuda/11.8.0
+module load opencv/4.8.0
+source /home/jquinto/projects/aip-gwtaylor/jquinto/virtualenvs/$ENV_NAME/bin/activate
 
 # Debugging outputs
 pwd
-which conda
 python --version
 pip freeze
 
 # LazyConfig Training Script - pretrained new baseline
 TILE_SIZE=512
-python tools/lazyconfig_train_net.py --num-gpus 4 \
+python tools/lazyconfig_train_net.py --num-gpus 2 \
 --resume \
 --config-file /h/jquinto/Mask-RCNN/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
 --exp_id ${TILE_SIZE} \
@@ -38,7 +34,7 @@ dataloader.train.dataset.names=lifeplan_${TILE_SIZE}_train \
 dataloader.test.dataset.names=lifeplan_${TILE_SIZE}_valid \
 
 # # LazyConfig Training Script - from scratch
-# python tools/lazyconfig_train_net.py --num-gpus 4 \
+# python tools/lazyconfig_train_net.py --num-gpus 2 \
 # --resume \
 # --config-file /h/jquinto/Mask-RCNN/configs/new_baselines/mask_rcnn_R_50_FPN_100ep_LSJ.py \
 # --exp_id ${TILE_SIZE} \
